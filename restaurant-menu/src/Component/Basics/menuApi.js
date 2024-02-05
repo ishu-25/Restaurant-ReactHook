bc9ffe8b5a6ad3e7dc3941595e7704c247d26e9b
ishu-25 <ishuchampaneria03.is25@gmail.com>
2024-02-05 15:53:43 +0530

    Restaurant-Menu

---
+++ restaurant-menu/src/Component/Basics/menuApi.js
@@ -0,0 +1,92 @@
+const Menu = [
+    {
+        id:1,
+        image: "images/maggi.jpg",
+        name: 'maggi',
+        category: 'breakfast',
+        price: '40₹',
+        description:'Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet.',
+    },
+
+    {
+        id: 2,
+        image: "images/allupakoida.jpg",
+        name: "allu pakoida",
+        category: "evening",
+        price: "20₹",
+        description:
+          "Aloo pakora is a popular Indian snack where sliced potatoes are dipped in spiced gram flour batter and deep fried to perfection. ",
+      },
+
+      {
+        id: 3,
+        image: "images/corn.jpg",
+        name: "corn",
+        category: "breakfast",
+        price: "30₹",
+        description:
+          "Corn is a tall annual cereal grass that is widely grown for its large elongated ears of starchy seeds. ",
+      },
+
+      {
+        id: 4,
+        image: "../images/chola.jpg",
+        name: "chola",
+        category: "lunch",
+        price: "100₹",
+        description:
+          "Chole Masala is a popular Punjabi dish where chickpeas are simmered in a spicy & tangy gravy. “Chole” is a Punjabi word for “chickpeas” and “masala” is a word for “spices” ",
+      },
+
+      {
+        id: 5,
+        image: "../images/pizza.jpg",
+        name: "pizza",
+        category: "evening",
+        price: "180₹",
+        description:
+          "The perfect meal. An open-faced pie using tomato sauce, cheese, and any matter of toppings. Tastes like heaven and is sometimes stated to be better than a relationship. ",
+      },
+
+      {
+        id: 6,
+        image: "../images/nonvegthali.jpg",
+        name: "Non-Veg Thali",
+        category: "dinner",
+        price: "180₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+
+      {
+        id: 7,
+        image: "../images/sweet.jpg",
+        name: "Sweets",
+        category: "dinner",
+        price: "60₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+
+      {
+        id: 8,
+        image: "../images/rajmarice.jpg",
+        name: "Rajma Rice",
+        category: "lunch",
+        price: "90₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+
+      {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name: "samaso",
+        category: "evening",
+        price: "20₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+]
+
+export default Menu
